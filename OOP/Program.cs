0242e53036f721663559226ea80e6b023fb2b29a
m.baghlani <miladbg45@gmail.com>
2025-03-04 11:22:13 +0330

    fix comment spelling

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,5 +1,7 @@
 ﻿using OOP.Encapsulation;
 
+namespace OOP;
+
 public static class Program
 {
     public static void Main(string[] args)
@@ -7,7 +9,7 @@ public static class Program
         // Encapsulation
         var newCar = new Benz("C200");
 
-        // Internal methods of the Benz class are not visible from outside of the calss
+        // Internal methods of the Benz class are not visible from outside of the class
         Console.WriteLine($"Car name is: {newCar.Name} which is readOnly");
         newCar.Start();
     }
