c9c996cb8dc43fecd3935b8623265cdeeb05461c
milad bg <miladbg45@gmail.com>
2025-03-09 00:17:20 +0330

    add inheritance sample

---
+++ Inheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Inheritance;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var newStudent = new Student("ali rezai", 21, 98551235);
+        newStudent.GetStudentInfo();
+
+        var newTeacher = new Teacher("ali abdi", 44, 123);
+        newTeacher.GetStudentInfo();
+    }
+}
